8b47b3cb5ce3d1db36231e03fde610874dd8d26e
Kirana Maharddhika <maharddhika@itpi.co.id>
2023-08-30 13:56:40 +0700

    GIT LOGIN

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = 'b2f1baee9a1c465f8855';
+const githubClientSecret = '6fd0a22baa2e9f3546f6b4079c59ca20587370e';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
